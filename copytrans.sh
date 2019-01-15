0ed328cf192a26593bb8314ce89052cb836fdba1
xuewei <maxuewei1995@126.com>
2019-01-15 18:41:50 +0800

    Update copytrans.sh

--- copytrans.sh
+++ copytrans.sh
@@ -3,9 +3,9 @@
 # Need https://github.com/soimort/translate-shell installed.
 # Need xsel or xclip installed.
 # Need notify-send installed.
+# Need python2 installed
 
-se=$(xsel -b -n -o | tr '\n' ' '| tr '\r' ' ' | sed 's/  / /g') # xclip -selection clipboard -o
-echo $se
+se=$(xsel -b -n -o) # xclip -selection clipboard -o
+se=$(python2 /path-to-CopyTrans/pre_text.py "$se")
 re=$(trans -b en:zh "$se")  # the 'trans' command is the installed translate-shell.
-echo $re
 notify-send "$se" "$re" -i accessories-dictionary -t 2000
