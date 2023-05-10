746540f70d25681a652d2502341ff3e8ad4c2e28
maxuewei2 <selwynma@protonmail.com>
2023-05-10 17:59:56 +0800

    fix: 行末不是-时，将\n替换为空格 	modified:   copytrans.sh

--- copytrans.sh
+++ copytrans.sh
@@ -7,6 +7,12 @@
 curdir=`dirname $0`
 
 selected=`xsel -b -n -o` # xclip -selection clipboard -o
-selected=`printf "$selected" | tr -d '\r' | sed 's/-$//g' | tr -d '\n'`
+selected=`printf "$selected" | tr -d '\r' | \
+  awk '{
+         subnum = sub("-$","",$0);
+         printf "%s", $0
+         if (subnum == 0)
+           printf " "
+  }'`
 translated=`${curdir}/trans -e bing -b en:zh "$selected"`  # the 'trans' command is the installed translate-shell.
 notify-send "$selected" "$translated" -t 2000
